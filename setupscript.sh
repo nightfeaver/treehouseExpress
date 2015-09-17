c2035e94fadc9e2e0a6947e9a521bde1165d314b
nightfeaver <nightfeaver99@gmail.com>
2015-09-17 21:25:04 +0100

    added the set up script

---
+++ setupscript.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install git
+sudo apt-get install zsh
+sudo apt-get install curl
+sudo apt-get install nodejs
+sudo apt-get install npm
+sudo curl -L http://install.ohmyz.sh | sh
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+sudo add-apt-repository -y ppa:webupd8team/sublime-text-2
+sudo npm install -g express
+sudo npm install -g nodemon
+sudo npm install -g node-inspector
+zsh
