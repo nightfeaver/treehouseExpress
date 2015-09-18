4e94e769d22e61bffe414c843376900d9ffb3d5d
nightfeaver <nightfeaver99@gmail.com>
2015-09-18 08:18:00 +0100

    added the construction message

--- src/app.js
+++ src/app.js
@@ -8,10 +8,16 @@ app.get('/',function(req,res){
 	res.send('<h1>Hi Jonny H Bomb Man</h1>');
 });
 
-app.get('/blog/:title',function(req, res) {
+app.get('/blog/:title?',function(req, res) {
 	var title=req.params.title;
+	if(title===undefined) {
+		res.send('Page is under construction');
+		res.status(503);
+
+	} else {
 	var post=posts[title];
 	res.send(post);
+	}
 })
 
 app.listen(3000,console.log('The front end server is running on port 3000'));
